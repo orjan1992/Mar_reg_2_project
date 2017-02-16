e0dfdd63381cabed8925dbfa2cba27426d07b860
orjan1992 <orjan1992@hotmail.com>
2017-02-16 17:56:41 +0100

    tar lang tid. second order filter.

--- init.m
+++ init.m
@@ -1,17 +1,16 @@
 clear
 eta_0 = [0 0 0];
 nu_0 =[0 0 0];
-%Par.tau_max = [1.03 0 0];
 Par.K_psi_init = 0.28;
 
 %%thrust for simulink sim
-Par.Thrust = [1 0 0; 0.5 0 0];
-Par.thrust_time = [0; 10];
+Par.Thrust = [1 0 0; -1 0 0];
+Par.thrust_time = [0; 50];
 
 %% outside workspace
 Par.Workspace.origin = [0, 0]';
-Par.Workspace.threshold = 2;
-Par.Workspace.scale = 100;
+Par.Workspace.threshold = 2.2;
+Par.Workspace.scale = 250;
 Par.Workspace.x_max_limit = 100;
 Par.Workspace.x_min_limit = -Par.Workspace.x_max_limit;
 Par.Workspace.y_min_limit = -Par.Workspace.x_max_limit;
@@ -24,20 +23,21 @@ Par.Workspace.y_max = Par.Workspace.y_max_limit - Par.Workspace.threshold;
 
 %% noise and dropout
 Par.SensNoiseEnabled = 1;
-Par.Freeze.Enable = 0;
-Par.Freeze.length = 2;
-Par.Freeze.threshold = 2.8;%3.0;
+Par.Freeze.Enable = 1;
+Par.Freeze.length = 1.5;
+Par.Freeze.threshold = 3.8;%3.0;
 
-Par.Step_size = 0.01;
-Par.Sample_time = 0.01;
+%Par.Step_size = 0.1;
+Par.Sample_time = 0.001;
 Par.Sample_rate = 1/Par.Sample_time;
 
 %% thruster
 Par.Thrust_lim = [1.03 2.5 0.98]';
 
 %% Lowpass filter
-Par.Lowpass.omega.c_u = 100;
+Par.Lowpass.omega.c_u = 10;
 Par.Lowpass.omega.c_v = Par.Lowpass.omega.c_u;
 Par.Lowpass.omega.c_psi = Par.Lowpass.omega.c_u;
 
 sim('main', 'FixedStep', num2str(Par.Sample_time));
+%sim main.slx
