ce84ca215e4b1d46f419389a10ece4311222b1ff
orjan1992 <orjan1992@hotmail.com>
2017-02-19 12:08:12 +0100

    edited plotting and changed parameters

--- NPO_init.m
+++ NPO_init.m
@@ -1,7 +1,7 @@
 %% Init file for NPO
 M = [16.79 0 0;0 15.79 0.554625;0 0.554625 2.76];
 NPO.invM = inv(M);
-NPO.Tinv = inv(10*diag([10 10 10]));
+NPO.Tinv = inv(100*diag([10 10 10]));
 NPO.A_w = ones(6, 6);
 NPO.C_w = ones(3, 6);
 NPO.eta_0 = [0 0 0];
@@ -11,4 +11,4 @@ scale = 1;
 NPO.K_2 = scale*diag([1.1 1.1 1.1]);
 
 NPO.K_4 = scale*diag(10*[0.1 0.1 0.1]);
-NPO.K_3 = 0*0.1*NPO.K_4;
+NPO.K_3 = 0.1*NPO.K_4;
